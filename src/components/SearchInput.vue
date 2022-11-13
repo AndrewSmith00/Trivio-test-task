a0cf4253f8067691473d3b008453cef90ee85ec1
Andrew <mr.andreika.0003@gmail.com>
2022-11-14 00:10:49 +0300

    Small type error fixed

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -20,7 +20,7 @@ export default defineComponent({
     computed: {
         search: {
             get() { return this.value; },
-            set(v) { this.$emit('input', v); }
+            set(v: string) { this.$emit('input', v); }
         }
     }
 })
--- src/components/SelectSort.vue
+++ src/components/SelectSort.vue
@@ -20,7 +20,7 @@ export default defineComponent({
     computed: {
         selected: {
             get() {return this.value},
-            set(v) {this.$emit('input', v)}
+            set(v: string) {this.$emit('input', v)}
         }
     },
 })
